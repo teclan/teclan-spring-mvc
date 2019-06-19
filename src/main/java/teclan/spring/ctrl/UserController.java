ec7e037d8030f596f7728b6854425ec32f550e1f
teclan <tbj621@163.com>
2019-06-19 16:03:29 +0800

    登录成功

---
+++ src/main/java/teclan/spring/ctrl/UserController.java
@@ -0,0 +1,36 @@
+package teclan.spring.ctrl;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import teclan.spring.util.HttpTool;
+import teclan.spring.util.ResultUtil;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Controller
+@RequestMapping(value = "/user")
+public class UserController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(UserController.class);
+
+
+    @RequestMapping(value = "/login")
+    @ResponseBody
+    public JSONObject test(HttpServletRequest request, HttpServletResponse response) {
+        try {
+            String json = HttpTool.readJSONString(request);
+            JSONObject parameter = JSON.parseObject(json);
+
+            LOGGER.info("\n\n登录信息: {}",parameter);
+
+        } catch (Exception e) {
+            LOGGER.error(e.getMessage(), e);
+        }
+        return ResultUtil.jsonResultList(200, "成功");
+    }
+}
--- src/main/webapp/resource/js/login/login.js
+++ src/main/webapp/resource/js/login/login.js
@@ -1,4 +1,6 @@
 
+
+
 function register(){
  alert('注册，未实现！');
 
@@ -6,5 +8,30 @@ function register(){
 
 
 function login(id,password){
- alert('登录,ID='+id+' ,password='+password);
+    alert('登录,ID='+id+' ,password='+password);
+
+     var handleSuccess = function(o){
+            if(o !== undefined) {
+                    try {
+                        var reseult = eval("("+o+")");
+                        if('1'==reseult.flag){
+                          return true; // 已收取
+                        }else{
+                            return false; // 未收取
+                        }
+                    } catch(e) {
+                        alert("error!"+e);
+                        return false;
+                    }
+           }
+      };
+
+ 	 var handleFailure = function(o){
+ 	 };
+
+    var json = '{"id":"'+id+'","password":"'+password+'"}';
+
+ 	sync('POST',BASE_URL+'/user/login.do?id='+id+'&password='+password,json,handleSuccess,handleFailure);
+
+
 };
