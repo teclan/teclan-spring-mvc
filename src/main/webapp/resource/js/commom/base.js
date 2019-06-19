60d1d4acee280e6f017e7176e5ed473367d3b125
teclan <tbj621@163.com>
2019-06-19 14:47:14 +0800

    登录页面雏形

---
+++ src/main/webapp/resource/js/commom/base.js
@@ -0,0 +1,3 @@
+<script  language="javascript" type="text/javascript"  src="resource/lib/js/jquery-3.4.1.min.js"></script>
+<script  language="javascript" type="text/javascript"  src="resource/lib/js/jquery-ajax-expand.js"></script>
+<script  language="javascript" type="text/javascript"  src="resource/lib/bootstrap-3.3.7/js/bootstrap.min.js"></script>
---
+++ src/main/webapp/resource/js/login/login.js
@@ -0,0 +1,10 @@
+
+function register(){
+ alert('注册，未实现！');
+
+};
+
+
+function login(id,password){
+ alert('登录,ID='+id+' ,password='+password);
+};
