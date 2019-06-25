1e8add2ef633eb5dc2c644879f31e195a2d06f4e
teclan <tbj621@163.com>
2019-06-25 15:32:19 +0800

    修改过滤器的参数问题

--- src/main/java/teclan/spring/filter/DataSyncFilter.java
+++ src/main/java/teclan/spring/filter/DataSyncFilter.java
@@ -1,8 +1,7 @@
 package teclan.spring.filter;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -13,12 +12,16 @@ import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.common.base.Strings;
+import com.mysql.cj.util.StringUtils;
+import org.elasticsearch.common.util.Consumer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 
 import teclan.spring.util.HttpTool;
+import teclan.spring.util.Objects;
 import teclan.spring.util.PropertyConfigUtil;
 
 public class DataSyncFilter implements Filter {
@@ -48,16 +51,25 @@ public class DataSyncFilter implements Filter {
 
 		String requestURI = request.getRequestURI();
 
-		String jsonStr = "";
-		if (request.getContentType() != null
-				&& request.getContentType().toLowerCase().contains("multipart/form-data")) {
-			CommonsMultipartResolver commonsMultipartResolver = new CommonsMultipartResolver();
-			MultipartHttpServletRequest multipartRequest = commonsMultipartResolver.resolveMultipart(request);
-			jsonStr = multipartRequest.getParameter("json");
-		} else {
-			jsonStr = HttpTool.readJSONString(request);
-		}
-		new Thread(new ProcessFilter(jsonStr, requestURI)).start();
+		Object para = "";
+
+
+		 if ("application/json".equalsIgnoreCase(request.getContentType())){
+			 para = HttpTool.readJSONParam(request);
+		}else {
+		 	Set<String> parameters = request.getParameterMap().keySet();
+
+			List<String> query = new ArrayList<>();
+
+			 Iterator<String> iterator =  parameters.iterator();
+
+			 while (iterator.hasNext()){
+			 	String m = iterator.next();
+				 query.add(String.format("%s=%s",m,request.getParameter(m)));
+			 }
+			 para = Objects.Joiner("&",query);
+		 }
+		new Thread(new ProcessFilter(para, requestURI)).start();
 	}
 
 	@Override
--- src/main/java/teclan/spring/filter/ProcessFilter.java
+++ src/main/java/teclan/spring/filter/ProcessFilter.java
@@ -8,6 +8,8 @@ import org.springframework.jdbc.core.JdbcTemplate;
 
 import com.alibaba.fastjson.JSONObject;
 
+import java.util.Map;
+
 public class ProcessFilter implements Runnable {
 	private static final Logger LOGGER = LoggerFactory.getLogger(ProcessFilter.class);
 	private static final ClassPathXmlApplicationContext appContextData;
@@ -16,10 +18,10 @@ public class ProcessFilter implements Runnable {
 	static {
 		appContextData = new ClassPathXmlApplicationContext("ApplicationContext/Application-mysql-data-source.xml");
 	}
-	private String requestParams;
+	private Object requestParams;
 	private String requestUrl;
 
-	public ProcessFilter(String requestParams, String requestUrl) {
+	public ProcessFilter(Object requestParams, String requestUrl) {
 		this.requestParams = requestParams;
 		this.requestUrl = requestUrl;
 	}
@@ -27,15 +29,14 @@ public class ProcessFilter implements Runnable {
 	@Override
 	public void run() {
 
-		JSONObject params=null;
-		try {
-		  params = JSONObject.parseObject(requestParams);
-		}catch(Exception e) {
-			LOGGER.error(e.getMessage(),e);
+		if(requestParams instanceof String){
+			LOGGER.info("2 ProcessFilter 监测到 URL={}?{}",requestUrl,requestParams);
+		}else {
+			LOGGER.info("2 ProcessFilter 监测到 URL={},参数:{}",requestUrl,requestParams.toString());
 		}
-		
-		// TODO
-		LOGGER.info("ProcessFilter 监测到 URL={},参数:{}",requestUrl,params);
+
+
+
 
 	}
 }
