cee5a3410d72fdfe1129636c9a9bf0e69020312f
teclan <tbj621@163.com>
2019-07-25 17:49:19 +0800

    添加echarts图表

--- src/main/webapp/resource/js/common/include.js
+++ src/main/webapp/resource/js/common/include.js
@@ -1,3 +1,6 @@
+document.write('<script src="../lib/echarts/echarts.js" type="text/javascript" charset="utf-8"></script>');
+
+
 document.write('<script src="../lib/js/jquery-3.4.1.min.js" type="text/javascript" charset="utf-8"></script>');
 document.write('<script src="../lib/js/jquery-ajax-expand.js" type="text/javascript" charset="utf-8"></script>');
 
@@ -6,6 +9,7 @@ document.write('<script src="../lib/messenger/js/messenger-theme-future.js" type
 
 document.write('<script src="../lib/bootstrap-3.3.7/js/bootstrap.min.js" type="text/javascript" charset="utf-8"></script>');
 document.write('<script src="../lib/bootstrapvalidator/js/bootstrapValidator.min.js" type="text/javascript" charset="utf-8"></script>');
+
 
 document.write('<script src="../js/common/config.js" type="text/javascript" charset="utf-8"></script>');
 document.write('<script src="../js/common/HttpTools.js" type="text/javascript" charset="utf-8"></script>');
